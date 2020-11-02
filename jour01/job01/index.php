56fbc1392ce2bab46006df1ce553241c8d1488b5
Gaspard <gaspard.lonchampt@laplateforme.io>
2020-11-02 15:18:57 +0100

    Job 01 terminé avec commentaires

---
+++ jour01/job01/index.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+
+<?php 
+$str="Hello Laplateforme"; // merci l'ancien énnoncé !
+$str2="Vive";
+$str3="!";
+
+echo $str2.$str = preg_replace ('/Hello/','',$str). " ". $str3.'<br>'; // chaine de caractère concatainé
+
+$val = 6; // valeur entière
+
+echo $val. '<br>';
+
+$val += 4; // addition
+
+echo $val. '<br>';
+
+$myBool = true; // valeur booléenne true affiche 1
+
+echo $myBool. '<br>';
+
+$myBool = false; // valeur booléenne false affiche un espace
+
+echo $myBool;
+?>
+
+
+
+</body>
+</html>
