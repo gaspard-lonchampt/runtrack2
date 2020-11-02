c39bce95d7e3293e7858a1726994e7a9f9473911
Gaspard <gaspard.lonchampt@laplateforme.io>
2020-11-02 15:49:22 +0100

    job02 terminé avec deux types de commentaires

---
+++ jour01/job02/index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+
+$var = "Hello LaPlateforme";
+
+echo $var. '<br>'; //voici un commentaire uniligne 
+
+echo "Hello LaPlateforme"; /* voici 
+un
+commmentaire
+multiligne */
+
+?>
+</body>
+</html>
